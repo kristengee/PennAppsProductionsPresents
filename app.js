b238d9f9d70abcf1e4311d0a197515907262a702
Ryan O'Gorman <rogorman9@gmail.com>
2015-09-05 20:53:23 -0400

    app.js mentor

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer'); 
+var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//app.use(multer()); // for parsing multipart/form-data
+
+app.set('view engine', 'hjs');
+
 //lets require/import the mongodb native drivers.
 var mongodb = require('mongodb');
 
@@ -7,17 +18,36 @@ var MongoClient = mongodb.MongoClient;
 // Connection URL. This is where your mongodb server is running.
 var url = 'mongodb://localhost:27017/users';
 
+var db;
+
 // Use connect method to connect to the Server
-MongoClient.connect(url, function (err, db) {
+MongoClient.connect(url, function (err, database) {
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
-    //HURRAY!! We are connected. :)
+
     console.log('Connection established to', url);
 
     // do some work here with the database.
-
+	db = database;
+	
     //Close connection
-    db.close();
   }
+});
+
+app.post('/create_user', function(req, res) {
+	console.log(req.body.username);
+});
+
+app.get('/', function(req, res) {
+	res.render('index', {
+		heading: 'Franz'
+	});
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
 });
